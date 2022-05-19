02495fcbfee52f0f67397ab3ee9c91d9193045d8
Frank Abanto <franabanto12@gmail.com>
2022-05-19 01:59:09 -0500

    gh-pages added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-// import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
 import Boton from "./componentes/Boton";
 import Pantalla from "./componentes/Pantalla";
 import BotonClear from "./componentes/BotonClear";
@@ -23,13 +22,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* <div className="freecodecamp-logo-contenedor">
-        <img
-          src={freeCodeCampLogo}
-          className="freecodecamp-logo"
-          alt="Logo de freeCodeCamp"
-        />
-      </div> */}
       <div className="contenedor-calculadora">
         <Pantalla input={input} />
         <div className="fila">
